797aa00ffd767728217654297dd0e596b4ee85dc
fabiopisa <pisanellifabio@gmail.com>
2021-05-27 15:27:01 +0200

    commit prova per le flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,12 @@
     <li></li>
     <li>{{card.title || card.name}}</li>
     <li>{{card.original_title || card.original_name}}</li>
-    <li>Lingua: {{card.original_language}}</li>
+    <li
+    v-if="card.original_language !== 'en' && card.original_language !== 'it'"
+    >Lingua: {{card.original_language}}</li>
+    <li
+    v-if="card.original_language === 'en' || card.original_language === 'it'"
+    >Lingua: <img src="../assets/img/flag-e+'imgs[card.original_language]'+.svg" alt="flag.en"></li>
     <li>Voto: {{card.vote_average}}</li>
   </ul>
 </template>
@@ -13,6 +18,14 @@ export default {
  name:'Card',
  props:{
    card: Object
+ },
+ data(){
+   return{
+     imgs:{
+       'en': 'en',
+       'it': 'it'
+     }
+   }
  }
 }
 </script>
@@ -25,6 +38,9 @@ ul{
     li{
       font-size: 13px;
       padding-right: 10px;
+      img{
+        width: 15px
+      }
     };
 };
 </style>
