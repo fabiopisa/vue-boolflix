f877d2e14585459a65b6a5f6a50ccfbdcd936f06
fabiopisa <pisanellifabio@gmail.com>
2021-05-31 12:36:33 +0200

    commit milestone 4 quasi finito

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
-  <ul class="col-12 mb-4 col-md-6 col-lg-4 col-xl-3">
-    <li>
+  <div class="big-box col-12 mb-4 col-md-6 col-lg-4 col-xl-3">
+    <div class="primary-box">
       <img  v-if="card.poster_path !== null" class="cover" :src="'https://image.tmdb.org/t/p/w342/'+card.poster_path" :alt="card.poster_path">
 
       <div class="not-found" v-if="card.poster_path === null">
@@ -22,15 +22,15 @@
         </li>
         <li class="star d-flex flex-row justify-content-center" v-html="mathStar(card)"></li>
         <li>
-          <h6>{{card.overview}}</h6>
+          <h5>{{card.overview}}</h5>
         </li>
         <li v-if="card.overview === ''">
           <h6>Nessuna descrizione trovata</h6>
         </li>
       </ul>
-    </li>
+    </div>
 
-  </ul>
+  </div>
 </template>
 
 <script>
@@ -68,73 +68,101 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-ul{
+div.big-box{
   color: #FFFFFF;
   padding: 0;
-  &:hover .back-card{
-    visibility: visible;
-    cursor: pointer; 
-    /* transition-property: opacity;
-    transition-duration: 1s;
-    transition-timing-function:ease-out;
-    transition-delay: 1s; */
-  }
-    li{
-      font-size: 22px;
-      line-height: 30px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      position: relative;
-      img.cover{
-        width: 90%;
-        height: 60vh;
-        margin-bottom:20px;
-        cursor: pointer;
+  position: relative;
+  div.primary-box{
+    font-size: 22px;
+    line-height: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: all 0.3s;
+    transition-timing-function: linear;
+    /* transition: height 3s ease-out, width 5s ease-in; */ 
+    &:hover{
+      width: 120%;
+      position: absolute;
+      z-index:6;
+      top:-5vh;
+      margin-left: -10%;
+      height: 70vh;
+    }
+    &:hover img.cover, .not-found, ul.back-card{
+      height: 70vh;
+    } 
+    &:hover .not-found{
+      height: 70vh;
+    } 
+    &:hover ul.back-card{
+      height: 70vh;
+      visibility: visible;
+      cursor: pointer;
+    } 
+    img.cover{
+      width: 90%;
+      height: 60vh;
+      margin-bottom:20px;
+      cursor: pointer;
+    }
+    .not-found{
+      width: 90%;
+      height: 60vh;
+      background-color: rgba(0,0,0,0.5);
+      text-align: center;
+      border: 1px solid #000000;
+      margin-bottom:20px;
+      h3{
+        color: #FFFFFF;
+        margin: 25px 0;
       }
-      .not-found{
-        width: 90%;
-        height: 60vh;
-        background-color: rgba(0,0,0,0.5);
+    }
+    ul.back-card{
+      padding: 0;
+      visibility: hidden;
+      position: absolute;
+      top: 0%;
+      width: 90%;
+      height: 60vh;
+      background-color: #000000;
+      opacity: 0.8;
+      overflow: auto;
+      transition: all 0.3s;
+      transition-timing-function: linear;
+      li{
         text-align: center;
-        border: 1px solid #000000;
-        margin-bottom:20px;
-        h3{
-          color: #FFFFFF;
-          margin: 25px 0;
+        line-height: 30px;
+        .flag{
+          width: 30px;
+          height: 20px;
+          margin-left: 5px;
+          margin-top:5px;
         }
-      }
-      .flag{
-        width: 30px;
-        margin-left: 5px;
-      }
-      &.star{
-        color: #FFBD00;
-      }
-      ul.back-card{
-        visibility: hidden;
-        position: absolute;
-        top: 0%;
-        left:5%;
-        width: 90%;
-        height: 60vh;
-        background-color: #000000;
-        opacity: 0.8;
-        overflow: auto;
-        li{
+        &.star{
+          color: #FFBD00;
+          margin-top: 10px;
+        }
+        &:first-child{
+          margin-top: 15px;
+        }
+        h5{
+          margin-top: 10px;
+          font-size: 15px;
+          line-height: 18px;
+          text-align: left;
+          padding: 0  10px;
+        }
+        h6{
+          margin-top: 10px;
+          font-size: 15px;
           text-align: center;
-          &:first-child{
-            margin-top: 15px;
-          }
-          h6{
-            margin-top: 10px;
-            font-size: 15px;
-            line-height: 18px;
-            text-align: left;
-            padding: 0  10px
-          }
         }
       }
-    };
+    }
+  };
 };
 </style>
